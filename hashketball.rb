7223b1f7015f771e653df277862873874aef9cbc
Alexander Schelchere <ascellie@gmail.com>
2019-05-17 09:45:27 -0400

    Skeleton for lab & bonuses uncommented

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,56 @@
 # Write your code here!
+def game_hash()
+
+end
+
+def num_points_scored()
+
+end
+
+def shoe_size()
+
+end
+
+def team_colors()
+
+end
+
+def team_names()
+
+end
+
+def player_numbers()
+
+end
+
+def player_stats()
+
+end
+
+def big_shoe_rebounds()
+
+end
+
+
+# bonus
+
+def most_points_scored()
+
+end
+
+def winning_team()
+
+end
+
+def player_with_longest_name()
+
+end
+
+# super bonus
+def long_name_steals_a_ton?()
+
+end
+
 
 
 
